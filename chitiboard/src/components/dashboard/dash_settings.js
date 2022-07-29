51ecc2ab950a1b2be7303725298cd2414c7a42c2
Jeigsaw <sumanull@outlook.com>
2022-07-29 18:36:34 -0400

    dash setting init

--- chitiboard/src/components/dashboard/dash_settings.js
+++ chitiboard/src/components/dashboard/dash_settings.js
@@ -1,14 +1,13 @@
 import { getFirestore, collection, doc, getDocs, addDoc, deleteDoc, updateDoc, onSnapshot } from 'firebase/firestore';
 import { FIREBASE_APP, database, authenticator } from '../../chiti_firebase';
 
-
 // dashboard main
 function Settings() {
 
     return (
       <main className='dash-main' id='dash-settings'>
         <h1>The site settings</h1>
-        {/* mainly home page ma display garne ka kuraharu: hero_img/title/text  number of posts, contact info and others*/}
+        
         
       </main>
     );
